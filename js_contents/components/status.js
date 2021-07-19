7916705c803a25669653d33683c3055a6819c1f4
Iqbal Badar <ibadar@ucsd.edu>
2021-07-18 20:24:57 -0700

    adding js contents for people to copy over

---
+++ js_contents/components/status.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+
+const endpoint = '/get_chain2'
+class Status extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      length: []
+   //   address: '',
+    }
+  }
+
+  componentDidMount() {
+    axios.get(endpoint)
+      .then(res => {
+        const length = res.data.length;
+        //const address = res.data.chain[1].transactions[0].receiver;
+        this.setState({ length 
+        //  ,address 
+        });
+      })
+  }
+  render(){
+    return (
+    <Container>
+    <br/>
+    <Row>
+    <Col sm="6">
+  <h5> <div><i className="fa fa-cubes"></i></div> No. of Blocks Mined </h5> <hr/>
+ <h5 style={{color: '#007bff'}}>#<b>{this.state.length} </b></h5>
+    </Col>
+    <Col md="6"> <br/>
+    <h5> <div>(sync <a href=""><i className="fa fa-refresh"></i></a> )</div></h5> <hr/>
+    </Col >
+    </Row>
+    <br/><br/><br/>
+    </Container>
+    );
+  }
+}
+
+export default Status;
